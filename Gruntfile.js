2c512bf613daf78de9f132e0ad7bf2db06d6cecb
diego <diego.sanches@gmail.com>
2015-01-28 15:03:38 -0300

    Gruntfile.js tidied up (2)

--- Gruntfile.js
+++ Gruntfile.js
@@ -153,13 +153,13 @@ module.exports = function(grunt) {
 	// A Task for loading the configuration object
 	grunt.task.registerTask('loadConfig', 'Task that loads the config into a grunt option.', function() {
 		var init = require('./config/init')();
-		
+
 		// It ignores files either for dev or prod
-	  // var config = require('./config/config');
-    
-	  // It loads every file included by the project regardless NODE_ENV
-	  // It makes sence because it's packing min file.
-	  var all_config = require('./config/env/all');
+		// var config = require('./config/config');
+
+		// It loads every file included by the project regardless NODE_ENV
+		// It makes sence because it's packing min file.
+		var all_config = require('./config/env/all');
 
 		grunt.config.set('applicationJavaScriptFiles', all_config.assets.js);
 		grunt.config.set('applicationCSSFiles', all_config.assets.css);
